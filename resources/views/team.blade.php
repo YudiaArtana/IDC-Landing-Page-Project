8bb629fb840ac3185290ae81c17ed16f83022e27
kadekadi123 <dekadi16juli@gmail.com>
2023-07-18 23:16:13 +0800

    Perbarui file composer.json, package.json, dan lainnya

---
+++ resources/views/team.blade.php
@@ -0,0 +1,304 @@
+@extends('layouts.master')
+@section('title', 'TEAM')
+
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- Tambahkan library AOS -->
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script>
+
+    <title>Document</title>
+</head>
+
+<body>
+    <section id="team" class="team section-bg">
+        <!-- IDC INTI -->
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h1 data-text="INSTIKI DEVELOPER CLUB">INSTIKI DEVELOPER CLUB</h1>
+                <h3>INTI</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-2 col-md-4-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/inti/ketua.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Natura</h4>
+                            <span>Ketua</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team//inti/wakil.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Dek mei</h4>
+                            <span>Ketua</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/inti/sekretaris_1.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>De oka</h4>
+                            <span>Ketua</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/inti/sekretaris_2.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Natura</h4>
+                            <span>Ketua</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/inti/bendahara_1.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Natura</h4>
+                            <span>Ketua</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-2 col-md-4-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/inti/bendahara_2.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Natura</h4>
+                            <span>Ketua</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END IDC INTI -->
+
+        <!-- IDC NON-DIVISI -->
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h3>NON DIVISI</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-6-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/non-divisi/ui_ux.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Marcell Budi</h4>
+                            <span>UI UX</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/non-divisi/web.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Tresna Agustina</h4>
+                            <span>Web Dev</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/non-divisi/mobile.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Rades Pratama</h4>
+                            <span>Mobile Dev</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/non-divisi/data_science.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Adrian Astalina</h4>
+                            <span>Data Science</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END IDC NON-DIVISI -->
+
+        <!-- IDC DIVISI -->
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h3>DIVISI</h3>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/divisi/pubdok.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Juniarta</h4>
+                            <span>Pubdok</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6-flex align-items-strecth">
+                    <div class="member text-center" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="img/team/divisi/humas.png" class="zoom-image img-fluid" alt="">
+                            <!-- social icon -->
+                            <div class="social">
+                                <a href="#"><i class="bi bi-facebook"></i></a>
+                                <a href="#"><i class="bi bi-linkedin"></i></a>
+                                <a href="#"><i class="bi bi-instagram"></i></a>
+                                <a href="#"><i class="bi bi-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Agus Pramana</h4>
+                            <span>Humas</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END IDC NON-DIVISI -->
+    </section>
+
+    <script>
+        AOS.init(
+        function animateFadeUp() {
+            var memberImgs = document.querySelectorAll('.member-img');
+
+            memberImgs.forEach(function (img) {
+                img.style.opacity = '0';
+                img.style.transform = 'translateY(-20px)';
+                img.style.transition = 'opacity 0.5s ease-out, transform 0.5s ease-out';
+
+                setTimeout(function () {
+                    img.style.opacity = '1';
+                    img.style.transform = 'translateY(0)';
+                }, 100);
+            });
+        });
+
+        window.addEventListener('load', animateFadeUp);
+        
+        
+
+    </script>
+</body>
+
+</html>
+
+
+@endsection
