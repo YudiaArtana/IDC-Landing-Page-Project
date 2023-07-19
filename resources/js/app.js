3628a0ae21c11d280553e8d040b38ff6c8ec5136
Dionanda <akunsayurbos@gmail.com>
2023-07-19 23:00:26 +0800

    Tambahan untuk tampilan News

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,16 +5,21 @@ import $ from 'jquery';
 
 // IMPORT BOOTSTRAP
 import '../sass/app.scss';  
-  
-// MY CSS
+
+// CSS
 import '../css/navbar.css'; 
 import '../css/home.css'; 
 import '../css/team.css'; 
 import '../css/news.css'; 
 import '../css/about.css'; 
 
+// JS
+import './news';
 
 // LIBRARY
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel/dist/owl.carousel.min.js';
+
 import Ukiyo from "ukiyojs";
 
 const parallax = document.querySelector('.ukiyo')
