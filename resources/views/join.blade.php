aa3d345cc8b9aeb9cc2325414683a79a17a90db4
Duta Kharisma <dutakharismaputra87@gmail.com>
2023-09-13 23:42:31 +0800

    Fix Join

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -8,45 +8,46 @@
 @section('title', 'Form Pendaftaran')
 
 @section('content')
-<div class="section-title d-flex justify-content-start align-items-center px-3">
-    <div class="container p-0">
+<div class="section-title d-flex justify-content-center align-items-center px-3">
+    <div class="container">
         <div class="title-join">
-            <div class="row pt-0 d-flex justify-content-start align-items-center">
-                <div class="col-lg-6 col-md-6 col-sm-12 tulisan d-flex justify-content-start align-items-start">
+            <div class="row d-flex justify-content-start align-items-center">
+                <div class="col-lg-6 col-md-6 col-sm-12 tulisan d-flex justify-content-start align-items-start" data-aos="fade-right" data-aos-delay="50" data-aos-duration="2000">
                     <h4>INSTIKI DEVELOPER CLUB</h4>
                     <h1>Let's, Join with us!</h1>
                     <p class="title-divisi">Chase your dream of becoming a <span class="auto-type "
                             id="auto-type"></span></p>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-12 gambar">
+                <div class="col-lg-6 col-md-6 col-sm-12 gambar" data-aos="fade-left" data-aos-delay="50" data-aos-duration="2000">
                     <img src="img/join/join.png" alt="">
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 <div class="container px-3">
 
     <form>
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">Email</label>
             <input type="email" class="form-control form-control-lg box-form rounded-0" id="exampleInputEmail1"
                 placeholder="Enter your Email" style="color: white">
         </div>
 
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">NIM</label>
             <input type="text" class="form-control form-control-lg box-form rounded-0" id="exampleInputEmail1"
                 placeholder="Enter your NIM" style="color: white">
         </div>
 
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">Name</label>
             <input type="text" class="form-control form-control-lg box-form rounded-0" id="exampleInputEmail1"
                 placeholder="Enter your name" style="color: white">
         </div>
 
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">Years</label>
             <select class="form-select form-control-lg box-select rounded-0">
                 <option class="color-select" value="1">2019</option>
@@ -57,7 +58,7 @@
             </select>
         </div>
 
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">Major</label>
             <select class="form-select form-control-lg box-select rounded-0">
                 <option class="color-select" value="1">TI-MDI</option>
@@ -70,7 +71,7 @@
         </div>
 
 
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">Division</label>
             <select class="form-select form-control-lg box-select rounded-0">
                 <option class="color-select" value="1">Web Development</option>
@@ -80,19 +81,19 @@
             </select>
         </div>
 
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">Address</label>
             <input type="text" class="form-control form-control-lg box-form rounded-0" id="exampleInputEmail1"
                 placeholder="Enter your address" style="color: white">
         </div>
 
-        <div class="mb-3">
+        <div class="mb-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">
             <label for="exampleInputEmail1" class="form-label">The reason you chose IDC</label>
             <input type="text" class="form-control form-control-lg box-form rounded-0" id="exampleInputEmail1"
                 placeholder="Enter your reason" style="color: white">
         </div>
 
-        <button type="submit" class="btn btn-danger px-5 mt-3">Submit</button>
+        <button type="submit" class="btn btn-danger px-5 mt-3" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1100">Submit</button>
     </form>
 
     <br><br>
